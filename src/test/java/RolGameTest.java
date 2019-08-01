82735336f05a0e683a3501b0647e8f068eab13f6
Munoz Dominguez <jomu@gft.com>
2019-08-01 12:48:53 +0200

    refactor test cases name to requirements control

--- src/test/java/RolGameTest.java
+++ src/test/java/RolGameTest.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class RolGameTest {
 
     @Test
-    public void gameHasAPlayer(){
+    public void RQ1_gameHasAPlayer(){
         RolGame game = new RolGame();
         Assert.assertEquals(null, game.getPlayer());
         game.addPlayer("ZoroHD");
@@ -15,29 +15,29 @@ public class RolGameTest {
     }
 
     @Test
-    public void twoHeroesToChose() {
+    public void RQ2_twoHeroesToChose() {
         Assert.assertEquals(2, RolGame.getHeroes().size());
     }
 
     @Test
-    public void aHeroTypeIsWarrior() {
+    public void RQ2_aHeroTypeIsWarrior() {
         Assert.assertEquals(true, RolGame.getHero("Warrior") instanceof Hero);
         Assert.assertEquals("Warrior", RolGame.getHero("Warrior").getType());
     }
 
     @Test
-    public void aHeroTypeIsWizard() {
+    public void RQ2_aHeroTypeIsWizard() {
         Assert.assertEquals(true, RolGame.getHero("Wizard") instanceof Hero);
         Assert.assertEquals("Wizard", RolGame.getHero("Wizard").getType());
     }
 
     @Test
-    public void oneEnemyInGame() {
+    public void RQ3_oneEnemyInGame() {
         Assert.assertEquals(1, RolGame.getEnemies().size());
     }
 
     @Test
-    public void aEnemyTypeIsOrc() {
+    public void RQ3_aEnemyTypeIsOrc() {
         Assert.assertEquals(true, RolGame.getEnemy("Orc") instanceof Enemy);
         Assert.assertEquals("Orc", RolGame.getEnemy("Orc").getType());
     }
